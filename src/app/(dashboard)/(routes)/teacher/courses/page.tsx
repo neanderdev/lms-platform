addb914935f1e803a0dfa99196209ebca87b6a79
Neander de Souza <neander.faria@hotmail.com>
2024-05-20 14:21:31 -0300

    Course Creation UI

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 export default function CoursesPage() {
     return (
-        <div>Courses Page</div>
+        <div className="p-6">
+            <Link href="/teacher/create">
+                <Button>New Course</Button>
+            </Link>
+        </div>
     );
 }
