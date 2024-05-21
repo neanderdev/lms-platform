114773342fbe176730182586fe01bf1febfea9e8
Neander de Souza <neander.faria@hotmail.com>
2024-05-21 10:51:49 -0300

    Course Edit Page UI

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,7 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  // matcher: ["/((?!.*..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  // matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
