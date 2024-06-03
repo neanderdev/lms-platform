f21fc6dcbd634723c53f33763c2c02abb6bc9163
Neander de Souza <neander.faria@hotmail.com>
2024-06-03 15:58:21 -0300

    Tabela de dados dos cursos

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,37 @@
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
-export default function CoursesPage() {
+import { db } from "@/lib/db";
+
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+
+export default async function CoursesPage() {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
     return (
         <div className="p-6">
             <Link href="/teacher/create">
                 <Button>New Course</Button>
             </Link>
+
+            <DataTable columns={columns} data={courses} />
         </div>
     );
 }
