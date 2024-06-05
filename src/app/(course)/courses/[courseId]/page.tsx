458b36bbe64daf2108557b1a88e7c7e3a3123aac
Neander de Souza <neander.faria@hotmail.com>
2024-06-05 10:15:03 -0300

    Chapter HLS Video Player

--- src/app/(course)/courses/[courseId]/page.tsx
+++ src/app/(course)/courses/[courseId]/page.tsx
@@ -1,5 +1,31 @@
-export default function CourseIdPage() {
-    return (
-        <div>Watch the course</div>
-    );
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+export default async function CourseIdPage({
+    params,
+}: {
+    params: { courseId: string };
+}) {
+    const course = await db.course.findUnique({
+        include: {
+            chapters: {
+                where: {
+                    isPublished: true,
+                },
+                orderBy: {
+                    position: "asc",
+                },
+            },
+        },
+        where: {
+            id: params.courseId,
+        },
+    });
+
+    if (!course) {
+        return redirect("/");
+    }
+
+    return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 }
